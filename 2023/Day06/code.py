efb7cfe281a98faf931641dcdc4fd470ba00aafe
Krisje Jordaens <kris.jordaens@esc.be>
2023-12-07 06:51:00 +0100

    Day 6

--- 2023/Day06/code.py
+++ 2023/Day06/code.py
@@ -8,19 +8,55 @@ input = []
 def readinput(filename):
     filename = f"{os.path.dirname(__file__)}\{filename}"
     global input
-    input = readinput_lines(filename)
+    ip = readinput_lines(filename)
+    for line in ip:
+        line = line.split(":")[1].strip()
+        line = list(map(int,line.split()))
+        input.append(line)
+
     
 def main():
    readinput("input.txt")
-   first_star()
+   #first_star()
    second_star()
 
 def first_star():
+    
+    wins = 1
+    for idx,time in enumerate(input[0]):
+        record =  input[1][idx]
+        wins*= len(getRecordBreaking(time,record))
+   
     print("Result First Star")
-    print(input)
- 
+    print(wins)
+
 def second_star():
+    time = ""
+    record = ""
+    for t in input[0]:
+        time += str(t)
+    for t in input[1]:
+        record += str(t)
+
+    
+    print(len(getRecordBreaking(int(time),int(record))))
     print("Result Second Star")
  
+def getRecordBreaking(time,record):
+    result = []
+    resttime = time
+    speed = 0
+    br = False
+    for i in range(1,time):
+        resttime -= 1
+        speed += 1
+        if speed*resttime > record:
+            br = True
+            result.append(i)
+        elif br:
+            break
+            
+    return result   
+
 if __name__ == '__main__':
     main()
