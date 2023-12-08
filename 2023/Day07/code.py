441f9f7baaa4771a3be4262dfba167f672039d8f
Krisje Jordaens <kris.jordaens@esc.be>
2023-12-08 13:24:33 +0100

    day 8

--- 2023/Day07/code.py
+++ 2023/Day07/code.py
@@ -1,26 +1,106 @@
 import math
 import functools ,itertools
 import os, sys
+from collections import Counter
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append("C:\DevOpps\GitHub\AdventOfCode")
 from  AOCHelper import * 
 input = []
+cards = defaultdict(int)
 def readinput(filename):
     filename = f"{os.path.dirname(__file__)}\{filename}"
     global input
     input = readinput_lines(filename)
-    
+    for line in input:
+        c,b = line.split()
+        cards[c] = int(b)
+
 def main():
    readinput("input.txt")
-   first_star()
+   #first_star()
    second_star()
 
 def first_star():
+    tot = 0
+    scores = defaultdict(int)
+    for card in cards:
+        scores[card] = getScore(card)
+   
+    scores = dict(sorted(scores.items(), key=lambda x:x[1]))
+    sort = defaultdict(str)
+    s = "23456789TJQKA"
+    r = "abcdefghijklm" 
+    for score in scores:
+        rs=str(scores[score])
+        for c in score:
+            rs+= r[s.index(c)]
+        sort[score] = rs
+    sort = dict(sorted(sort.items(), key=lambda x:x[1]))
+    for idx,sor in enumerate(sort):
+        tot += cards[sor] * (idx + 1)
+        
     print("Result First Star")
-    print(input)
- 
+    print(tot)
+
 def second_star():
+    tot = 0
+    scores = defaultdict(int)
+    for card in cards:
+        scores[card] =  max(map(getScore,getReplacements(card)))
+   
+    scores = dict(sorted(scores.items(), key=lambda x:x[1]))
+   
+    sort = defaultdict(str)
+    s = "J23456789TQKA"
+    r = "abcdefghijklm" 
+    for score in scores:
+        rs=str(scores[score])
+        for c in score:
+            rs+= r[s.index(c)]
+        sort[score] = rs
+    sort = dict(sorted(sort.items(), key=lambda x:x[1]))
+    for idx,sor in enumerate(sort):
+        tot += cards[sor] * (idx + 1)
+        
     print("Result Second Star")
- 
+    print(tot)
+
+def getReplacements(card):
+    if card == "":
+        return [""]
+
+    return [
+        x + y
+        for x in ("23456789TQKA" if card[0] == "J" else card[0])
+        for y in getReplacements(card[1:])
+    ]
+
+
+def getScore(card)->int:
+    # 7 = Five of a kind,
+    ct = Counter(card)
+    if len(ct) == 1:
+        return 7
+    if len(ct) == 2:
+        if max(ct.values()) == 4:
+            return 6
+        else: return 5
+    if len(ct) == 3:
+        if max(ct.values()) == 3:
+            return 4
+        else: return 3
+    if len(ct) == 4:
+        return 2
+    
+    # High card
+    s = "23456789TJQKA"
+    idx= s.index(card[0]) 
+    for c in card[1::]:
+        if s.index(c) != idx+1:
+            return 0
+        idx = s.index(c)
+    
+    return 1        
+
 if __name__ == '__main__':
     main()
--- 2023/Day08/code.py
+++ 2023/Day08/code.py
@@ -1,26 +1,88 @@
 import math
 import functools ,itertools
 import os, sys
+import collections
+from math import gcd
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append("C:\DevOpps\GitHub\AdventOfCode")
 from  AOCHelper import * 
 input = []
+dirs = defaultdict(str)
 def readinput(filename):
     filename = f"{os.path.dirname(__file__)}\{filename}"
     global input
     input = readinput_lines(filename)
+    for line in input[2:]:
+        a,b = line.split("=")
+        b=b.replace('(','').replace(')','').split(",")
+        dirs[a.strip()] = (b[0].strip(),b[1].strip())
     
 def main():
    readinput("input.txt")
-   first_star()
+   #first_star()
    second_star()
 
 def first_star():
+    instructs = collections.deque([s for s in (input[0])])
+    ins = instructs.popleft()
+    instructs.append(ins)
+    ct = 0
+    s = "AAA"
+    while s != "ZZZ":
+        ct+=1
+        s = dirs[s]["LR".index(ins)]
+        ins = instructs.popleft()
+        instructs.append(ins)
+    
     print("Result First Star")
-    print(input)
+    print(ct)
  
 def second_star():
+    instructs = collections.deque([s for s in (input[0])])
+    ins = instructs.popleft()
+    instructs.append(ins)
+    counts = []
+    for s in  [x for x in dirs if x[2] == "A"]:
+        ct = 0
+        while s[2] != "Z":
+            ct+=1
+            s = dirs[s]["LR".index(ins)]
+            ins = instructs.popleft()
+            instructs.append(ins)
+        counts.append(ct)
+    
+    tot = 1
+    for num in counts:
+        tot = tot * num // gcd(tot, num)
     print("Result Second Star")
- 
+    print(tot)
+
+def chines():
+    cr  = ChineseReminder()
+    instructs = collections.deque([s for s in (input[0])])
+    ins = instructs.popleft()
+    instructs.append(ins)
+
+    paths =[]
+    ends = []
+    for line in dirs:
+        for ins in instructs:
+            s  = dirs[line]["LR".index(ins)]
+            i=0
+            m=10000
+            for c in s:
+                print(ord(c),c)
+                i += ord(c) * m
+                m = m / 100
+
+            paths.append(i)
+            if s.endswith("Z"):
+                ends.append(i)
+    print(cr.calculate_chinese_remainder(paths,ends))
+
+
+    ct = 0
+    s = "AAA"
 if __name__ == '__main__':
     main()
