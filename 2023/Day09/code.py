2f67b2e2f6d8c57f824f3e823387d7fd5e779350
Krisje Jordaens <kris.jordaens@esc.be>
2023-12-09 11:07:00 +0100

    Day 9

--- 2023/Day09/code.py
+++ 2023/Day09/code.py
@@ -12,15 +12,53 @@ def readinput(filename):
     
 def main():
    readinput("input.txt")
-   first_star()
+   #first_star()
    second_star()
 
 def first_star():
+    #1939607041 to High
+    #1939607039
+    tot = 0
+    for line in input:
+        start = [[int(i) for i in line.split()]]
+        extra = start
+        while(True):
+            new = getExtraPolation(extra)
+            start.append(new)
+            extra = [new]
+            if all(x==0 for x in new):
+                break
+        for ex in start:
+            tot+= ex[-1]
+    
     print("Result First Star")
-    print(input)
+    print(tot)
  
 def second_star():
+    tot = 0
+    for line in input:
+        start = [[int(i) for i in line.split()]]
+        extra = start
+        while(True):
+            new = getExtraPolation(extra)
+            start.append(new)
+            extra = [new]
+            if all(x==0 for x in new):
+                break
+        
+        diff = 0
+        for i in range(len(start)-1,1,-1):
+            diff = start[i-1][0] - diff
+           
+        tot+=start[0][0] - diff
+
     print("Result Second Star")
- 
+    print(tot)
+def getExtraPolation(seq):
+    seq2 = []
+    for idx,i in enumerate(seq[0][:len(seq[0])-1]):
+        seq2.append(seq[0][idx+1] - i)
+    return seq2      
+  
 if __name__ == '__main__':
     main()
