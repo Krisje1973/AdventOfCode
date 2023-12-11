5d36cf29ffd91f759529b540b6258746c58d0cfe
Krisje Jordaens <kris.jordaens@esc.be>
2023-12-11 19:52:13 +0100

    cleanup

--- 2023/Day11/code.py
+++ 2023/Day11/code.py
@@ -5,32 +5,12 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append("C:\DevOpps\GitHub\AdventOfCode")
 from  AOCHelper import * 
 input = []
-galaxies =[]
 def readinput(filename):
     filename = f"{os.path.dirname(__file__)}\{filename}"
     global input,galaxies,empty_rows,empty_cols
     input = readinput_lines(filename)
-    empty_rows = []
-    empty_cols = []
-    galaxies = []
-   
-    for ri in range(len(input)):
-        h = [input[ri][x]=="." for x in range(len(input[0]))]  
-        if all(c for c in h):
-            empty_rows.append(ri)
-    for ci, col in enumerate(input[0]):
-        v = [input[x][ci]=="." for x in range(len(input))]  
-        if all(c for c in v):
-           empty_cols.append(ci)
-
-    for r, row in enumerate(input): 
-        for c, ch in enumerate(row):
-            if ch == "#":
-                galaxies.append((r,c))
-
     empty_rows = [r for r, row in enumerate(input) if all(ch == "." for ch in row)]
     empty_cols = [c for c, col in enumerate(zip(*input)) if all(ch == "." for ch in col)]
-
     galaxies = [(r, c) for r, row in enumerate(input) for c, ch in enumerate(row)  if ch == "#"]
        
 def main():
