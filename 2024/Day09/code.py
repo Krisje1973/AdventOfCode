c0a8c9a190c47ead99e6bf669ebe481202c1d45c
Krisje Jordaens <kris.jordaens@esc.be>
2024-12-09 19:55:30 +0100

    day9 p1

--- 2024/Day09/code.py
+++ 2024/Day09/code.py
@@ -6,12 +6,19 @@ sys.path.append("C:\DevOps\github\AdventOfCode")
 
 from AOCHelper import * 
 input = []
+spaces = []
+files = []
 
 def readinput(filename):
     filename = f"{os.path.dirname(__file__)}\{filename}"
     global input
+    global files
+    global spaces
    
-    input = readinput_lines(filename)
+    input = list(map(int,readinput_lines(filename)[0]))
+    files = [x for i,x in enumerate(input) if i % 2 == 0]
+
+    spaces = [x for i,x in enumerate(input) if i % 2 != 0]
     
 def main():
    readinput("input.txt")
@@ -20,8 +27,30 @@ def main():
 
 def first_star():
     result = 0
-  
+    ids = len(files) -1
+    file_list = [ids-id for id,file in enumerate(files[::-1]) for _ in range(file)]
+    empties = [0 for s in spaces for _ in range(s)]   
+    for i,_ in enumerate(empties):
+        empties[i] = file_list[i]
+    
+    empties.reverse()
+    end = len(file_list)
+    full = []
+   
+    for i,num in enumerate(input):
+        if len(full) == end: break
+        if i % 2 == 0:
+            for j in range(num):
+                full.append(file_list.pop())
+        else:
+            for j in range(num):
+                if len(empties) > 0:    
+                    full.append(empties.pop())
+                else: full.append(file_list.pop())
+   
     print("Result First Star")
+    for i,val in enumerate(full[:end]):
+        result += i*val
     print(result)
 
 def second_star():
