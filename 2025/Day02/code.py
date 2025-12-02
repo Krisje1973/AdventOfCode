c17dd5208ca3db8b9af81a7c37da20e41a48a51b
Krisje Jordaens <kris.jordaens@sde.be>
2025-12-02 22:46:11 +0100

    Day 2

--- 2025/Day02/code.py
+++ 2025/Day02/code.py
@@ -1,5 +1,3 @@
-import math
-import functools ,itertools
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append("C:\DevOpps\GitHub\AdventOfCode")
@@ -8,19 +6,41 @@ input = []
 def readinput(filename):
     filename = f"{os.path.dirname(__file__)}\{filename}"
     global input
-    input = readinput_lines(filename)
+    input = readinput_lines(filename)[0].split(",")
     
 def main():
    readinput("input.txt")
-   first_star()
+   #first_star()
    second_star()
-
+ 
 def first_star():
+    result = 0
+    for id in input:
+        s,e = map(int,id.split("-"))
+        for i in range(s,e+1):
+            s = str(i)
+            l = len(s) // 2
+            if s[:l] == s[l:]:
+                result += i
+                
     print("Result First Star")
-    print(input)
- 
+    print(result)
+
 def second_star():
+    result = 0
+    for id in input:
+        s,e = map(int,id.split("-"))
+        for i in range(s,e+1): 
+            s = str(i)
+            len_s = len(s)
+            l = len(s) // 2
+            for j in range(1,l+1):
+                c = s.count( s[:j])
+                if c == len_s / j:
+                    result += i
+                    break
+
     print("Result Second Star")
- 
+    print(result)
 if __name__ == '__main__':
     main()
