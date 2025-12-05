89444de33ce93aab2c718dc2fc95e1c85924e6fd
Krisje Jordaens <kris.jordaens@sde.be>
2025-12-05 06:44:01 +0100

    Day 4

--- 2025/Day04/code.py
+++ 2025/Day04/code.py
@@ -12,15 +12,45 @@ def readinput(filename):
     
 def main():
    readinput("input.txt")
-   first_star()
+   #first_star()
    second_star()
 
 def first_star():
+    gh = GridHelper()
+    maxx = len(input[0])
+    maxy = len(input)
+    result = 0
+    for row in range(maxy):
+        for col in range(maxx):
+            if input[row][col] != "@":
+                continue
+            sur = gh.get_adjacent_pos_with_diag(row,col,maxx,maxy)
+            if sum([int(input[r][c] == "@") for r,c in sur]) < 4:
+                result += 1
     print("Result First Star")
-    print(input)
+    print(result)
  
 def second_star():
+    global input
+    gh = GridHelper()
+    maxx = len(input[0])
+    maxy = len(input)
+    result = 0
+    moved = True
+    seen = set()
+    while moved:
+        moved = False
+        for row in range(maxy):
+            for col in range(maxx):
+                if input[row][col] != "@" or (row,col) in seen:
+                    continue
+                sur = gh.get_adjacent_pos_with_diag(row,col,maxx,maxy)
+                if sum([int(input[r][c] == "@" and (r,c) not in seen) for r,c in sur]) < 4:
+                    result += 1
+                    seen.add((row,col))
+                    moved = True
+    
     print("Result Second Star")
- 
+    print(result)
 if __name__ == '__main__':
     main()
