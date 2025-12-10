8efcc6099245cbb721bd1fbe24cfb409d1f7dab3
Krisje Jordaens <kris.jordaens@sde.be>
2025-12-10 19:45:35 +0100

    Day 9

--- 2025/Day09/code.py
+++ 2025/Day09/code.py
@@ -12,15 +12,34 @@ def readinput(filename):
     
 def main():
    readinput("input_ex.txt")
-   first_star()
+   #first_star()
    second_star()
 
 def first_star():
-    print("Result First Star")
-    print(input)
- 
+    tiles = [list(map(int, line.strip().split(','))) for line in input]
+   
+    result=0
+    for x, y in tiles:
+        for xx, yy in tiles:
+            result=max(result, (abs(x-xx)+1)*(abs(y-yy)+1))
+   
+    print(result)
+
 def second_star():
+    tiles = [list(map(int, line.strip().split(','))) for line in input]
+    xs = defaultdict(list)
+    ys = defaultdict(list)
+    for tile in tiles:
+        x,y = tile
+        xs[x].append(y)
+        ys[y].append(x)
+
+    result=0
+    grid = []
+    for x in xs:
+        xs[x].sort()
+    
     print("Result Second Star")
- 
+    print(result)
 if __name__ == '__main__':
     main()
